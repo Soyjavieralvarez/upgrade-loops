0a2ede3e9c427f1a2982c15a2fed3ffe81d27880
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-01-03 20:40:03 +0100

    comenzamos

---
+++ ex2.js
@@ -0,0 +1,2 @@
+const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de Gansta', 'Camiseta de Basket', 'Cinturón de Orión', 'AC/DC Camiseta'];
+console.log(products.includes('Camiseta'));
