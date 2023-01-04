adbd984231449ed1dce88dffb6e12ecc365ce552
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-01-04 08:38:00 +0100

    seguimos

--- ex2.js
+++ ex2.js
@@ -25,3 +25,65 @@ const alumns = [
     {name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
     {name: 'Raquel Benito', T1: true, T2: true, T3: true},
 ]
+
+for (let i = 0; i<alumns.length; i++){
+    name = alumns[i];
+
+    let approvedSubjects = 0;
+    approvedSubjects = name.T1 ? approvedSubjects + 1: approvedSubjects;
+    approvedSubjects = name.T2 ? approvedSubjects + 1: approvedSubjects;
+    approvedSubjects = name.T3 ? approvedSubjects + 1: approvedSubjects;
+    name.isApproved = approvedSubjects >= 2 ? true : false;
+
+}
+
+console.log(alumns);
+
+//!Interacción 3
+
+//?Usa un bucle forof para recorrer todos los destinos del array. Imprime en un clg sus valores. 
+
+const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar'];
+
+for (const places of placesToTravel) {
+    console.log(places);
+}
+
+//!Interacción 4
+
+//?Usa forin para imprimir por consola los datos del alinígena. 
+
+let alien = {
+    name: 'Chispita',
+    race: 'Pleyadianos',
+    planet: 'Pléyades',
+    weight: '100kg'
+}
+
+for (let dates in alien) {
+    console.log('This alien is: ' + dates + alien[dates]);
+}
+
+//!Interacción 5
+
+//?Usa un bulce for para recorrer todos los destinos del array y elimina los elementos que tengan el id 11 y 40. Imprime en clg el array. 
+
+const placesToTravel2 = [
+    {id: 5, name: 'Japan'},
+    {id: 11, name: 'Venecia'},
+    {id: 23, name: 'Murcia'},
+    {id: 40, name: 'Santander'},
+    {id: 44, name: 'Filipinas'},
+    {id: 59, name: 'Madagascar'}]
+
+for (let i = 0; i < placesToTravel2.length; i++){
+    const element = placesToTravel2[i];
+    if (element.id === 11 || element.id === 40) {
+        placesToTravel2.splice(i, 1);
+    }
+}
+
+console.log(placesToTravel2);
+
+
+
